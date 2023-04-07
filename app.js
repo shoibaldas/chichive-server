1e8434242c22c96d85829e0104da6021a0090d29
Shoibal Das <your_email@example.com>
2023-04-08 01:28:35 +0600

    json fake data is initialzied, API has been created, and vercel has been setup and deployed

--- app.js
+++ app.js
@@ -6,18 +6,23 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const products = require('./data/products.json');
+const categories = require('./data/categories.json');
 
 app.get('/', (req, res) => {
     res.send('Chichive API is running');
 });
 
-app.get('/proudcts', (req, res) => {
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/products', (req, res) => {
     res.send(products);
 });
 
-app.get('/products/:id', (req, res) => {
+app.get('/categories/:id', (req, res) => {
     const id = req.params.id;
-    const getProduct = products.find(n => n._id === id);
+    const getProduct = products.filter(n => n.category_id === id);
     res.send(getProduct);
 });
 
